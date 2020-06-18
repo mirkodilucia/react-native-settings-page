45f2af532ddcb420c79a5442685e00fa80b6f831
Mirko Di Lucia <dlmrk95@gmail.com>
2020-06-18 18:15:43 +0200

    Fixed icon style

--- react-native-settings-page/src/Components/NavigateRow.js
+++ react-native-settings-page/src/Components/NavigateRow.js
@@ -20,15 +20,19 @@ class NavigateRow extends Component {
             <TouchableOpacity onPress={this.props.onPressCallback}>
                 <View style={containerInSection}>
                     <View style={containerInnerSection}>
+                        <View style={ iconLeft }>
                         {
                             (this.props.iconLeft) && this.props.iconLeft
                         }
+                        </View>
                         <Text style={text} numberOfLines={1} ellipsizeMode={'tail'}>
                             {this.props.text}
                         </Text>
+                        <View style={ iconRight }>
                         {
                             (this.props.iconRight) && this.props.iconRight
                         }
+                        </View>
                     </View>
                 </View>
             </TouchableOpacity>
